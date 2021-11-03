368972320449e5dda5e1cfe7acae704fca6e5e2f
nbarr <nbarrameda89@gmail.com>
2021-11-03 12:21:27 -0400

    aa_questions updated

--- aa_questions.rb
+++ aa_questions.rb
@@ -1,1 +1,30 @@
+require 'sqlite3'
+require 'singleton'
 
+class QuestionsDatabase < SQLite3::Database
+    include Singleton
+
+
+
+end
+
+class Users
+    attr_accessor :id, :fname, :lname
+
+    def initialize(options)
+        @id = options['id']
+        @fname = options['fname']
+        @lname = options['lname']
+    end
+
+    def create
+        raise "#{self} is already in database" if self.id
+        QuestionsDatabase.instance.execute(<<-SQL, self.fname, self.lname)
+            INSERT INTO
+                users (fname, lname)
+            VALUES
+                (?, ?)
+        SQL
+    end
+
+end
